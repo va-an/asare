559fef95289679c64e5a02d2dba4538c927b23b7
Vadim Anufriev <mailbox@vaan.io>
2021-04-24 09:57:08 +0300

    backend - config class

---
+++ asare-backend/backend/src/main/scala/io.vaan.asare/config/loader.scala
@@ -0,0 +1,24 @@
+package io.vaan.asare.config
+
+import ciris._
+import ciris.refined._
+import eu.timepit.refined.types.net.UserPortNumber
+import eu.timepit.refined.types.string.NonEmptyString
+import eu.timepit.refined.auto._
+import cats.implicits._
+import cats.effect.Async
+import eu.timepit.refined.string
+
+object load {
+  def apply[F[_]: Async]: F[Config] =
+    (
+      (env("API_HOST") or prop("apiHost")).as[NonEmptyString].option,
+      (env("API_PORT") or prop("apiPort")).as[UserPortNumber].option
+    ).parMapN { (apiHost, apiPort) =>
+        Config(
+          apiHost = apiHost getOrElse "0.0.0.0",
+          apiPort = apiPort getOrElse 8090
+        )
+      }
+      .load[F]
+}
