6ddf11cdacdadef412b665f1a0300ad21b38eab6
Vadim Anufriev <mailbox@vaan.io>
2021-12-29 23:18:22 +0300

    backend - users routes skeleton

--- asare-backend/backend/src/app.rs
+++ asare-backend/backend/src/app.rs
@@ -1,3 +1,4 @@
+use crate::users::routes::{create_user, login_user};
 use crate::{conf::Config, rebalancer::routes::rebalance_request};
 use actix_web::{middleware, web, App, HttpServer};
 use async_trait::async_trait;
@@ -14,6 +15,11 @@ impl AsareHttpServer for ActixHttpServer {
         HttpServer::new(|| {
             App::new()
                 .service(web::scope("/v4/rebel/").service(rebalance_request))
+                .service(
+                    web::scope("/v1/users/")
+                        .service(create_user)
+                        .service(login_user),
+                )
                 .wrap(middleware::Logger::default())
         })
         .bind((config.http_host, config.http_port))?
---
+++ asare-backend/backend/src/users/routes.rs
@@ -0,0 +1,20 @@
+use actix_web::{http::Error, post, web, HttpResponse, Resource};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct CreateUserRequest {
+    name: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct LoginUserRequest;
+
+#[post("/create")]
+pub async fn create_user(req: web::Json<CreateUserRequest>) -> Result<HttpResponse, Error> {
+    todo!();
+}
+
+#[post("/login")]
+pub async fn login_user(req: web::Json<LoginUserRequest>) -> Result<HttpResponse, Error> {
+    todo!();
+}
